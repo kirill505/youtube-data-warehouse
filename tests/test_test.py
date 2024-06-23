fa2e9577b6e1f2bece85382a375a34137229ca09
Kirill Bazarov <kl.bazarov@gmail.com>
2024-06-23 18:41:22 -0300

    Issue-002: add docs

--- tests/test_test.py
+++ tests/test_test.py
@@ -5,6 +5,8 @@ def test_calc_addition():
 
 
 def test_calc_multiplication():
-    # Verify the output of addition two const
+    """
+    Verify the output of addition two const
+    """
     output = 6 * 3
     assert output == 18
