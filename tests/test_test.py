b473af5014be67bd7f26e22f72949e5006306bb8
Kirill Bazarov <kl.bazarov@gmail.com>
2024-06-23 14:53:58 -0300

    Issue-001: add one more test

--- tests/test_test.py
+++ tests/test_test.py
@@ -2,3 +2,9 @@ def test_calc_addition():
     # Verify the output of addition two const
     output = 2 + 4
     assert output == 6
+
+
+def test_calc_multiplication():
+    # Verify the output of addition two const
+    output = 6 * 3
+    assert output == 18
