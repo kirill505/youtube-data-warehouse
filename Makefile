85b8777f5ea2f12f1f7ef9c882965e448bfa2143
Kirill Bazarov <kl.bazarov@gmail.com>
2024-06-23 08:09:52 -0300

    minor changes

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 up:
-	docker compose --env-file .env up --build -d
+	docker compose --env-file .env up --build
 
 etl:
 	docker exec etl python /app/src/yt_subs_parsing/parsing_yt_sub.py -r ./data/raw -u https://www.youtube.com/watch?v=uF76d5EmdtU
--- src/yt_subs_parsing/parsing_yt_sub.py
+++ src/yt_subs_parsing/parsing_yt_sub.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import pandas as pd
 from youtube_transcript_api import YouTubeTranscriptApi
+# from pytube import YouTube
 
 
 def yt_parse_url_params(yt_url: str):
@@ -14,6 +15,19 @@ def yt_parse_url_params(yt_url: str):
     return params
 
 
+def video_Info(yt_link):
+    yt = YouTube(yt_link)  # Create Youtube Object..
+    res = {}
+
+    res["Title"] = yt.title
+    res["Length"] = yt.length
+    res["Views"] = yt.views
+    res["Restricted"] = yt.age_restricted
+    res["Rating"] = round(yt.rating)
+    res["Thumbnail_Url"] = yt.thumbnail_url
+    return res
+
+
 def yt_get_subtitles(yt_video_id: str):
     srt = YouTubeTranscriptApi.get_transcript(f"{yt_video_id}", languages=['en', 'es'])
     df = pd.json_normalize(srt)
@@ -22,20 +36,21 @@ def yt_get_subtitles(yt_video_id: str):
 
 def yt_save_subs_to_csv(df: pd.DataFrame, root_path: str, filename: str):
     df["youtube_id"] = os.path.basename(filename).split('.')[0]
+    print(f"{root_path}/{filename}")
     df.to_csv(f"{root_path}/{filename}", index=False)
 
 
-def main(params):
-    yt_video_url = params.yt_video_url
+def fetch_youtube_subtitles(yt_video_url: str, root_path: str):
+    yt_video_url = yt_video_url
 
     yt_video_id = yt_parse_url_params(yt_video_url)["v"]
     yt_subs = yt_get_subtitles(yt_video_id)
-    yt_save_subs_to_csv(yt_subs, params.root_path, f"{yt_video_id}.csv")
+    yt_save_subs_to_csv(yt_subs, root_path, f"{yt_video_id}.csv")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--yt_video_url", type=str)
-    parser.add_argument("-r", "--root_path", type=str)
-    args = parser.parse_args()
-    main(args)
+# if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-u", "--yt_video_url", type=str)
+    # parser.add_argument("-r", "--root_path", type=str)
+    # args = parser.parse_args()
+    # fetch_youtube_subtitles("https://www.youtube.com/watch?v=gsKuETFJr54", "")
