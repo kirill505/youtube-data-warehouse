91ef1b2c88ef7c3c8080c039be90e37d32e55dec
Kirill Bazarov <kl.bazarov@gmail.com>
2024-06-23 07:41:38 -0300

    pipeline: minor changes

--- pipeline/main_pipeline.py
+++ pipeline/main_pipeline.py
@@ -16,9 +16,23 @@ def main():
     engine = create_conn()
 
     """ Landing area """
-    file_path = os.getenv('FILE_PATH')
+    file_path = os.getenv('SAVE_PATH')
+
+
     table_name = os.getenv('TABLE_NAME')
-    df = pd.read_csv(file_path)
+    files  = [file for file in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, file))]
+    print(file_path, files)
+
+    df = pd.DataFrame()
+    for file in files:
+        file_url = os.path.join(file_path, file)
+        df_appended = pd.read_csv(file_url)
+        # data.append(df)
+        # print(len(data), data)
+
+        df = pd.concat([df, df_appended])
+
+    # df = pd.read_csv(file_path)
     print(df)
     load_table_to_landing(df, engine, table_name)
 
